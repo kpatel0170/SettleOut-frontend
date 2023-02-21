32d3cf3e09816ff0c84fdc7cf0e7dda82e06f154
spm126 <122588899+spm126@users.noreply.github.com>
2023-02-21 16:32:42 -0600

    Added navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,1 +1,25 @@
 
+import React from 'react';
+// import Logo from "../assets/Globalblac.png";
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <div style={{display: "flex", justifyContent: "space-between"}}>
+        <div>
+            {/* <img style={{height: "15q0px"}} src={Logo} alt="Logo" />             */}
+        </div>
+        <div>
+            <ul style={{display: "flex"}}>
+            <a href="/Upgrade">Apply to be agent</a>
+            <a href="#">About Us</a>
+            <a href="/dashboard">Contact Us</a>
+                <a href="/signup">Sign Up</a>
+                </ul>
+        </div>
+    </div>
+      );
+    };
+
+    export default Navbar;
+

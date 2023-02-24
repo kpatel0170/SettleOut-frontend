06180b2e03a6e6bdd227dd7aec89d9feba8aac5f
Om Dalwadi <88398870+OmDalwadi@users.noreply.github.com>
2023-02-24 12:54:41 -0600

    Setting the context component

---
+++ src/components/context/Context.js
@@ -0,0 +1,35 @@
+
+import { createContext, useEffect, useReducer } from "react";
+import Reducer from "./Reducer";
+
+const INITIAL_STATE = {
+  user: JSON.parse(localStorage.getItem("user")),
+  token: JSON.parse(localStorage.getItem("token")),
+  isFetching: false,
+  error: false
+};
+
+export const Context = createContext(INITIAL_STATE);
+
+export const ContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(Reducer, INITIAL_STATE);
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(state.user));
+
+    localStorage.setItem("token", JSON.stringify(state.token));
+  }, [state.user, state.token]);
+  return (
+    <Context.Provider
+      value={{
+        user: state.user,
+        isFetching: state.isFetching,
+        error: state.error,
+        token: state.token,
+        dispatch
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
---
+++ src/components/context/Reducer.js
@@ -0,0 +1,44 @@
+const Reducer = (state, action) => {
+    switch (action.type) {
+      case "LOGIN_START":
+        return {
+          user: null,
+          isFetching: true,
+          token: null,
+          error: false
+        };
+      case "LOGIN_SUCCESS":
+        return {
+          user: action.payload,
+          isFetching: false,
+          token: action.token,
+          error: false
+        };
+      case "LOGIN_FAILURE":
+        return {
+          user: null,
+          token: null,
+          isFetching: false,
+          error: true
+        };
+      case "LOGOUT": {
+        return {
+          user: null,
+          token: null,
+          isFetching: false,
+          error: false
+        };
+      }
+      case "UPDATE_USER":
+        return {
+          user: action.payload,
+          isFetching: false,
+          error: false,
+          token: state.token
+        };
+      default:
+        return state;
+    }
+  };
+  
+  export default Reducer;

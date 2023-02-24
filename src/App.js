71c2add716022bcdeb4e2eac10c96813faf566a8
Om Dalwadi <88398870+OmDalwadi@users.noreply.github.com>
2023-02-24 13:56:43 -0600

    Added BFF for Login and few fixes in general

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
+
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import { Route } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import About from "./pages/About";
 import Payment from "./pages/Payment";
@@ -13,34 +12,26 @@ import Card from './pages/Card';
 import Button from './pages/Card';
 import AgentList from './pages/AgentList';
 import ADBoard from './pages/ADBoard';
+import { Context } from "./components/context/Context";
+import { useContext } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import './App.css';
 
 function App() {
+      const { user, dispatch, token } = useContext(Context);
   return (
     
 
-    // So in general please uncomment the particular page that you are working on so that it reflects when you do npm start
-<<<<<<< HEAD
-     //   <About />
-=======
-       // <About />
->>>>>>> 76ca17a404321657943458c59167cba1285256a3
-      //  <Login />
-      // <Signup />  // Uncomment this and comment the login so that you can see signup page
-       <Navbar />
-       //<Card />
-      // <Button />
-<<<<<<< HEAD
-      <ADBoard />
-=======
-    //<UserDetail />
->>>>>>> 76ca17a404321657943458c59167cba1285256a3
-      // <AgentList />
-      //<Payment />
-      //<Upgrade/>
-      //<PreArrival/>
-      //<Homepage/>
+      <BrowserRouter>
+      <Routes>
+        <Route
+          exact
+          path="/login"
+          element={<Login />}
+        />
+        </Routes>
+        </BrowserRouter>
   );
 }
 

e7df7f1db00b953dd025f64f028834c643c99774
Om Dalwadi <88398870+OmDalwadi@users.noreply.github.com>
2023-02-24 14:57:03 -0600

    Error fix

--- src/App.js
+++ src/App.js
@@ -12,14 +12,11 @@ import Card from './pages/Card';
 import Button from './pages/Card';
 import AgentList from './pages/AgentList';
 import ADBoard from './pages/ADBoard';
-<<<<<<< HEAD
 import FeedbackForm from './pages/FeedbackForm';
 
-=======
 import { Context } from "./components/context/Context";
 import { useContext } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
->>>>>>> b723b17805e691b5dc8aef9c2afc15c3a85c68f8
 
 import './App.css';
 
@@ -28,30 +25,6 @@ function App() {
   return (
     
 
-<<<<<<< HEAD
-    // So in general please uncomment the particular page that you are working on so that it reflects when you do npm start
-//<<<<<<< HEAD
-     //   <About />
-//=======
-       // <About />
-//>>>>>>> 76ca17a404321657943458c59167cba1285256a3
-      //  <Login />
-      // <Signup />  // Uncomment this and comment the login so that you can see signup page
-       //<Navbar />
-       //<Card />
-      // <Button />
-//<<<<<<< HEAD
-     // <ADBoard />
-//=======
-    //<UserDetail />
-//>>>>>>> 76ca17a404321657943458c59167cba1285256a3
-      // <AgentList />
-      //<Payment />
-      //<Upgrade/>
-      //<PreArrival/>
-     // <Homepage/>
-      < FeedbackForm/>
-=======
       <BrowserRouter>
       <Routes>
         <Route
@@ -61,7 +34,6 @@ function App() {
         />
         </Routes>
         </BrowserRouter>
->>>>>>> b723b17805e691b5dc8aef9c2afc15c3a85c68f8
   );
 }
 

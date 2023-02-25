58fe5cacfba6b394c04156181d77b3e3a28f1a40
Om Dalwadi <88398870+OmDalwadi@users.noreply.github.com>
2023-02-25 01:56:45 -0600

    Added BFF in ADBoard with few test sample

--- src/pages/ADBoard/ADBoard.jsx
+++ src/pages/ADBoard/ADBoard.jsx
@@ -1,81 +1,91 @@
-import React, { useState, useEffect } from 'react';
+import React from "react";
+import { Fragment } from "react";
 import "./ADBoard.css";
 
 
-function ADBoard  ({ featured = false, children = null }){return (
+const agentData = [
+  {
+      id: 1,
+      name: "Om Dalwadi",
+      bio: "BSc(Honors) in Computer Science.",
+      rating: 4.5,
+      feedbacks: ["Great agent!", "Very professional"],
+  },
+  {
+      id: 2,
+      name: "Kartik Patel",
+      bio: "BSc in Computer Science.",
+      rating: 3.8,
+      feedbacks: ["Could improve communication"],
+  },
+  {
+      id: 3,
+      name: "Yug Shah",
+      bio: "BSc(Honors) in Computer Science.",
+      rating: 4.2,
+      feedbacks: [
+          "Very knowledgeable about the market",
+          "Always available to answer questions",
+      ],
+  },
+];
+
+
+const ADBoard = () => {
+  const handleSelectAgent = (id) => {
+      console.log(`Agent with ID ${id} selected`);
+  };
+
+  const handleViewProfile = (id) => {
+      const agent = agentData.find((agent) => agent.id === id);
+      const feedback = agent.feedbacks.join(", ");
+
+      const feedbackContainer = document.createElement("div");
+      feedbackContainer.innerHTML = `
+    <h2 class="text-lg font-medium mb-2">${agent.name} Feedback</h2>
+    <p class="text-gray-600">${feedback}</p>
+    <button
+      class="bg-blue-500 text-white px-3 py-2 rounded mt-4"
+      onclick="this.parentElement.remove()"
+    >
+      Go Back
+    </button>
+  `;
+
+      document.body.appendChild(feedbackContainer);
+  };
+
+
+return (
 <>
 <div className="container">
     <ul className="cards">
-      <li className="card">
+    {agentData.map((agent) => (
+      <li className="card" key={agent.id}>
+     
         <div>
           <div className="row">
             <div className="column"> </div>
             <div className="column">
-              <h3 className="card-title"><b>John Doe</b></h3>
+              <h3 className="card-title"><b>{agent.name}</b></h3>
+              <div>
+               <h4>Ratings: {agent.rating}</h4>
+              </div>
               <br></br>
             </div>
           </div>
           <div className="card-content">
-            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."</p>
+            <p>{agent.bio}</p>
           </div>
         </div>
         <div className="card-link-wrapper">
-          <a href="" className="wp-block-button__link has-base-color has-text-color has-text-align-center wp-element-button card-link">Tasks</a>
+          <a href=""  onClick={() => handleSelectAgent(agent.id)} className="wp-block-button__link has-base-color has-text-color has-text-align-center wp-element-button card-link">Appoient</a>
         </div>
       </li>
-      <li className="card">
-        <div>
-          <div className="row">
-            <div className="column"> </div>
-            <div className="column">
-              <h3 className="card-title"><b>John Doe</b></h3>
-              <br></br>
-            </div>
-          </div>
-          <div className="card-content">
-            <p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.”</p>
-          </div>
-        </div>
-        <div className="card-link-wrapper">
-          <a href="" className="wp-block-button__link has-base-color has-text-color has-text-align-center wp-element-button card-link">Tasks</a>
-        </div>
-      </li>
-      <li className="card">
-        <div>
-          <div className="row">
-            <div className="column"> </div>
-            <div className="column">
-              <h3 className="card-title"><b>John Doe</b></h3>
-              <br></br>
-            </div>
-          </div>
-          <div className="card-content">
-            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."</p>
-          </div>
-        </div>
-        <div className="card-link-wrapper">
-          <a href="" className="wp-block-button__link has-base-color has-text-color has-text-align-center wp-element-button card-link">Tasks</a>
-        </div>
-      </li>
-   
-      <li className="card">
-        <div>
-          <div className="row">
-            <div className="column">  </div>
-            <div className="column">
-              <h3 className="card-title"><b>John Doe</b></h3>
-              <br></br>
-            </div>
-          </div>
-          <div className="card-content">
-            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."</p>
-          </div>
-        </div>
-        <div className="card-link-wrapper">
-          <a href="" className="wp-block-button__link has-base-color has-text-color has-text-align-center wp-element-button card-link">Tasks</a>
-        </div>
-      </li>
+      ))}
+      
     </ul>
+   
   </div>
 
 
@@ -83,6 +93,6 @@ function ADBoard  ({ featured = false, children = null }){return (
 
 
     );
-}
+};
 
 export default ADBoard;

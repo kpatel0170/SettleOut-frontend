39413f2259c85e158da33025d3788291ffc3191f
kpatel0170 <kartikpatel0170@gmail.com>
2024-01-13 09:03:51 -0600

    feat: update public and private routing

--- src/App.js
+++ src/App.js
@@ -10,65 +10,109 @@ import Signup from "./components/Signup";
 import About from "./pages/About";
 import Payment from "./pages/Payment";
 import Membership from "./pages/Cards";
-import Upgrade from "./pages/Upgrade";
+import Upgrade from "./pages/AUpgrade";
 import PreArrival from "./pages/PreArrival";
 import UserDetail from "./pages/UserDetail";
 import Homepage from "./pages/Homepage";
 import Button from "./components/Button";
-import AgentList from "./pages/AgentList";
-import ADBoard from "./pages/ADBoard";
+import AgentList from "./pages/AList";
+import ADBoard from "./pages/ABoard";
 import FeedbackForm from "./pages/FeedbackForm";
-import StudentTask from "./pages/StudentTask";
+import StudentTask from "./pages/ATasks";
 import ContactUs from "./pages/ContactUs";
-import Agprofile from "./pages/agProfile/Agprofile";
+import AProfile from "./pages/AProfile/AProfile";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 
-
 function App() {
-    const { user, token } = useContext(Context);
-  
-    return (
-      <>
-        <ReactNotifications />
-        <BrowserRouter>
+  const { user, token } = useContext(Context);
+
+  return (
+    <>
+      <ReactNotifications />
+      <BrowserRouter>
         <Navbar />
-          <Routes>
-            {/* Public Routes */}
-            <Route path="/login" element={token && user ? <Login /> : <Login />} />
-            <Route path="/signup" element={token && user ? <Signup /> : <Signup />} />
-            <Route path="/aboutus" element={token && user ? <About /> : <Login />} />
-            <Route path="/contactus" element={token && user ? <ContactUs /> : <ContactUs />} />
-  
-            {/* Private Routes */}
-            <Route
-              path="/"
-              element={
-                token && user ? (
-                  user.accountType === "public" ? <Homepage /> : <ADBoard />
+        <Routes>
+          {/* Public Routes */}
+          <Route
+            path="/login"
+            element={token && user ? <Login /> : <Login />}
+          />
+          <Route
+            path="/signup"
+            element={token && user ? <Signup /> : <Signup />}
+          />
+          <Route
+            path="/aboutus"
+            element={token && user ? <About /> : <Login />}
+          />
+          <Route
+            path="/contactus"
+            element={token && user ? <ContactUs /> : <ContactUs />}
+          />
+
+          {/* Private Routes */}
+          <Route
+            path="/"
+            element={
+              token && user ? (
+                user.accountType === "public" ? (
+                  <Homepage />
                 ) : (
-                  <Login />
+                  <ADBoard />
                 )
-              }
-            />
-            <Route path="/upgrade" element={token && user ? <Upgrade /> : <Login />} />
-            <Route path="/payment" element={token && user ? <Payment /> : <Login />} />
-            <Route path="/agprofile" element={token && user ? <Agprofile /> : <Login />} />
-            <Route path="/details" element={token && user ? <UserDetail /> : <Login />} />
-            <Route path="/feedback" element={token && user ? <FeedbackForm /> : <Login />} />
-            <Route path="/preArrival" element={token && user ? <PreArrival /> : <Login />} />
-            <Route path="/membership" element={token && user ? <Membership /> : <Login />} />
-  
-            {/* Other Private Routes */}
-            <Route path="/agent-list" element={token && user ? <AgentList /> : <Login />} />
-            <Route path="/agent-board" element={token && user ? <ADBoard /> : <Login />} />
-            <Route path="/tasks" element={token && user ? <StudentTask /> : <Login />} />
-          </Routes>
-          <Footer />
-        </BrowserRouter>
-      </>
-    );
-  }
-  
-  export default App;
-  
+              ) : (
+                <Login />
+              )
+            }
+          />
+          <Route
+            path="/upgrade"
+            element={token && user ? <Upgrade /> : <Login />}
+          />
+          <Route
+            path="/payment"
+            element={token && user ? <Payment /> : <Login />}
+          />
+          <Route
+            path="/aprofile"
+            element={token && user ? <AProfile /> : <Login />}
+          />
+          <Route
+            path="/details"
+            element={token && user ? <UserDetail /> : <Login />}
+          />
+          <Route
+            path="/feedback"
+            element={token && user ? <FeedbackForm /> : <Login />}
+          />
+          <Route
+            path="/preArrival"
+            element={token && user ? <PreArrival /> : <Login />}
+          />
+          <Route
+            path="/membership"
+            element={token && user ? <Membership /> : <Login />}
+          />
+
+          {/* Other Private Routes */}
+          <Route
+            path="/agent-list"
+            element={token && user ? <AgentList /> : <Login />}
+          />
+          <Route
+            path="/agent-board"
+            element={token && user ? <ADBoard /> : <Login />}
+          />
+          <Route
+            path="/tasks"
+            element={token && user ? <StudentTask /> : <Login />}
+          />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
